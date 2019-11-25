6f7a7864bb45aafbba686ac3ab401c3a17df5e1d
ImDaeseong <cs93059@gmail.com>
2019-11-25 09:27:32 +0900

    DaeseongBot

--- golang/telegram.go
+++ golang/telegram.go
@@ -7,12 +7,303 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 //https://web.telegram.org/#/im?p=@BotFather
 var bot *tgbotapi.BotAPI
+
+func getfileNameUrl(sUrl string) string {
+
+	nIndex := strings.LastIndex(sUrl, "/")
+	if nIndex == -1 {
+		return ""
+	}
+
+	return string(sUrl[nIndex+1:])
+}
+
+func AudioDownload(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+
+	url, err := bot.GetFileDirectURL(message.Audio.FileID)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	filename := getfileNameUrl(url)
+	if filename == "" {
+		return
+	}
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		return
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		file.Close()
+		return
+	}
+	file.Write(bytes)
+	file.Close()
+}
+
+func PhotoDownload(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+
+	photos := *message.Photo
+	photoId := photos[1].FileID
+	url, err := bot.GetFileDirectURL(photoId)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	filename := getfileNameUrl(url)
+	if filename == "" {
+		return
+	}
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		return
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		file.Close()
+		return
+	}
+	file.Write(bytes)
+	file.Close()
+}
+
+func DocumentDownload(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+
+	url, err := bot.GetFileDirectURL(message.Document.FileID)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	filename := getfileNameUrl(url)
+	if filename == "" {
+		return
+	}
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		return
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		file.Close()
+		return
+	}
+	file.Write(bytes)
+	file.Close()
+}
+
+func StickerDownload(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+
+	url, err := bot.GetFileDirectURL(message.Sticker.FileID)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	filename := getfileNameUrl(url)
+	if filename == "" {
+		return
+	}
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		return
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		file.Close()
+		return
+	}
+	file.Write(bytes)
+	file.Close()
+}
+
+func VideoDownload(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+
+	url, err := bot.GetFileDirectURL(message.Video.FileID)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	filename := getfileNameUrl(url)
+	if filename == "" {
+		return
+	}
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		return
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		file.Close()
+		return
+	}
+	file.Write(bytes)
+	file.Close()
+}
+
+func VoiceDownload(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+
+	url, err := bot.GetFileDirectURL(message.Voice.FileID)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	filename := getfileNameUrl(url)
+	if filename == "" {
+		return
+	}
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		return
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		file.Close()
+		return
+	}
+	file.Write(bytes)
+	file.Close()
+}
+
+func SendAudio(bot *tgbotapi.BotAPI, chatid int64, sPath string) {
+	sSend := tgbotapi.NewAudioUpload(chatid, sPath)
+	_, err := bot.Send(sSend)
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
+
+func SendPhoto(bot *tgbotapi.BotAPI, chatid int64, sPath string) {
+	sSend := tgbotapi.NewPhotoUpload(chatid, sPath)
+	_, err := bot.Send(sSend)
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
+
+func SendSticker(bot *tgbotapi.BotAPI, chatid int64, sPath string) {
+	sSend := tgbotapi.NewStickerUpload(chatid, sPath)
+	_, err := bot.Send(sSend)
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
+
+func SendStickerShare(bot *tgbotapi.BotAPI, chatid int64) {
+	sSend := tgbotapi.NewStickerShare(chatid, "CAADBQADYwUAAmQK4AW3jYFjDvykkAI")
+	_, err := bot.Send(sSend)
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
+
+func SendVideo(bot *tgbotapi.BotAPI, chatid int64, sPath string) {
+	sSend := tgbotapi.NewVideoUpload(chatid, sPath)
+	_, err := bot.Send(sSend)
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
+
+func SendVoice(bot *tgbotapi.BotAPI, chatid int64, sPath string) {
+	sSend := tgbotapi.NewVoiceUpload(chatid, sPath)
+	_, err := bot.Send(sSend)
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
+
+func SendDocument(bot *tgbotapi.BotAPI, chatid int64, sPath string) {
+	sSend := tgbotapi.NewDocumentUpload(chatid, sPath)
+	_, err := bot.Send(sSend)
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
 
 func main() {
 
@@ -43,309 +334,129 @@ func main() {
 
 		if update.Message.Command() == "/start" || update.Message.Text == "/start" {
 
-			sMsg := fmt.Sprintf("%s\n%s\n%s", "/help -> 나의 이미지", "/keyboard -> 나의 페이지", "/link -> 나의 링크 페이지")
-			sSendMsg := tgbotapi.NewMessage(chatid, sMsg)
-			_, err := bot.Send(sSendMsg)
-			if err != nil {
-				log.Print(err.Error())
-			}
+			go func() {
+				sMsg := fmt.Sprintf("%s\n%s\n%s\n%s", "/help -> 나의 이미지", "/keyboard -> 나의 페이지", "/link -> 나의 링크 페이지", "/close -> 키보드 제거")
+				sSendMsg := tgbotapi.NewMessage(chatid, sMsg)
+				_, err := bot.Send(sSendMsg)
+				if err != nil {
+					log.Print(err.Error())
+				}
+			}()
 
 		} else if update.Message.Command() == "/keyboard" || update.Message.Text == "/keyboard" {
 
-			button1 := tgbotapi.NewKeyboardButton("https://github.com/ImDaeseong")
-			button2 := tgbotapi.NewKeyboardButton("https://github.com/ImDaeseong/DaeseongBot")
+			go func() {
+				button1 := tgbotapi.NewKeyboardButton("https://github.com/ImDaeseong")
+				button2 := tgbotapi.NewKeyboardButton("https://github.com/ImDaeseong/DaeseongBot")
 
-			//한줄에
-			//Markup := tgbotapi.NewReplyKeyboard(tgbotapi.NewKeyboardButtonRow(button1, button2))
+				//한줄에
+				//Markup := tgbotapi.NewReplyKeyboard(tgbotapi.NewKeyboardButtonRow(button1, button2))
 
-			//한줄에
-			//Markup := tgbotapi.NewReplyKeyboard([]tgbotapi.KeyboardButton{button1, button2})
+				//한줄에
+				//Markup := tgbotapi.NewReplyKeyboard([]tgbotapi.KeyboardButton{button1, button2})
 
-			//여러줄에
-			Markup := tgbotapi.NewReplyKeyboard(tgbotapi.NewKeyboardButtonRow(button1), tgbotapi.NewKeyboardButtonRow(button2))
+				//여러줄에
+				Markup := tgbotapi.NewReplyKeyboard(tgbotapi.NewKeyboardButtonRow(button1), tgbotapi.NewKeyboardButtonRow(button2))
 
-			sMsg := "github.com/ImDaeseong 정보를 확인하세요."
-			sSendMsg := tgbotapi.NewMessage(chatid, sMsg)
-			sSendMsg.ReplyMarkup = Markup
-			_, err := bot.Send(sSendMsg)
-			if err != nil {
-				log.Print(err.Error())
-			}
+				sMsg := "github.com/ImDaeseong 정보를 확인하세요."
+				sSendMsg := tgbotapi.NewMessage(chatid, sMsg)
+				sSendMsg.ReplyMarkup = Markup
+				_, err := bot.Send(sSendMsg)
+				if err != nil {
+					log.Print(err.Error())
+				}
+			}()
 
 		} else if update.Message.Command() == "/link" || update.Message.Text == "/link" {
 
-			link1 := tgbotapi.NewInlineKeyboardButtonURL("Daeseong", "https://github.com/ImDaeseong")
-			link2 := tgbotapi.NewInlineKeyboardButtonURL("DaeseongBot", "https://github.com/ImDaeseong/DaeseongBot")
+			go func() {
+				link1 := tgbotapi.NewInlineKeyboardButtonURL("Daeseong", "https://github.com/ImDaeseong")
+				link2 := tgbotapi.NewInlineKeyboardButtonURL("DaeseongBot", "https://github.com/ImDaeseong/DaeseongBot")
 
-			//한줄에
-			Markup := tgbotapi.NewInlineKeyboardMarkup(tgbotapi.NewInlineKeyboardRow(link1, link2))
+				//한줄에
+				Markup := tgbotapi.NewInlineKeyboardMarkup(tgbotapi.NewInlineKeyboardRow(link1, link2))
 
-			//여러줄에
-			//Markup := tgbotapi.NewInlineKeyboardMarkup(tgbotapi.NewInlineKeyboardRow(link1), tgbotapi.NewInlineKeyboardRow(link2))
+				//여러줄에
+				//Markup := tgbotapi.NewInlineKeyboardMarkup(tgbotapi.NewInlineKeyboardRow(link1), tgbotapi.NewInlineKeyboardRow(link2))
 
-			sMsg := "나의 링크 정보를 확인하세요."
-			sSendMsg := tgbotapi.NewMessage(chatid, sMsg)
-			sSendMsg.ReplyMarkup = Markup
-			_, err := bot.Send(sSendMsg)
-			if err != nil {
-				log.Print(err.Error())
-			}
+				sMsg := "나의 링크 정보를 확인하세요."
+				sSendMsg := tgbotapi.NewMessage(chatid, sMsg)
+				sSendMsg.ReplyMarkup = Markup
+				_, err := bot.Send(sSendMsg)
+				if err != nil {
+					log.Print(err.Error())
+				}
+			}()
 
 		} else if update.Message.Command() == "/help" || update.Message.Text == "/help" {
 
-			imgUrl := "https://avatars0.githubusercontent.com/u/10001221?s=460&v=4"
-			sSendMsg := tgbotapi.NewPhotoShare(chatid, imgUrl)
-			_, err := bot.Send(sSendMsg)
-			if err != nil {
-				log.Print(err.Error())
-			}
+			go func() {
+				imgUrl := "https://avatars0.githubusercontent.com/u/10001221?s=460&v=4"
+				sSendMsg := tgbotapi.NewPhotoShare(chatid, imgUrl)
+				_, err := bot.Send(sSendMsg)
+				if err != nil {
+					log.Print(err.Error())
+				}
+			}()
+
+		} else if update.Message.Text == "/close" {
+
+			go func() {
+				sMsg := "키보드를 삭제합니다."
+				sSendMsg := tgbotapi.NewMessage(chatid, sMsg)
+				sSendMsg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+				_, err := bot.Send(sSendMsg)
+				if err != nil {
+					log.Print(err.Error())
+				}
+			}()
 
 		} else if update.Message.Text == "/send" {
 
-			fmt.Println("테스트 완료")
+			go SendAudio(bot, chatid, "D:\\DaeseongGolang\\src\\file_42.mp3")
 
-			/*
-				sSend := tgbotapi.NewAudioUpload(chatid, "E:\\GoApp\\src\\audio.mp3")
-				_, err := bot.Send(sSend)
-				if err != nil {
-					log.Print(err.Error())
-				}
-			*/
+			go SendPhoto(bot, chatid, "D:\\DaeseongGolang\\src\\file_43.jpg")
 
-			/*
-				sSend := tgbotapi.NewPhotoUpload(chatid, "E:\\GoApp\\src\\photo.jpg")
-				_, err := bot.Send(sSend)
-				if err != nil {
-					log.Print(err.Error())
-				}
-			*/
+			go SendSticker(bot, chatid, "D:\\DaeseongGolang\\src\\file_41.tgs")
 
-			/*
-				sSend := tgbotapi.NewStickerUpload(chatid, "E:\\GoApp\\src\\sticker.tgs")
-				_, err := bot.Send(sSend)
-				if err != nil {
-					log.Print(err.Error())
-				}
-			*/
+			go SendStickerShare(bot, chatid)
 
-			/*
-				sSend := tgbotapi.NewStickerShare(chatid, "CAADBQADYwUAAmQK4AW3jYFjDvykkAI")
-				_, err := bot.Send(sSend)
-				if err != nil {
-					log.Print(err.Error())
-				}
-			*/
+			go SendVideo(bot, chatid, "D:\\DaeseongGolang\\src\\file_47.mp4")
 
-			/*
-				sSend := tgbotapi.NewVideoUpload(chatid, "E:\\GoApp\\src\\video.mp4")
-				_, err := bot.Send(sSend)
-				if err != nil {
-					log.Print(err.Error())
-				}
-			*/
+			go SendVoice(bot, chatid, "D:\\DaeseongGolang\\src\\file_45.dat")
 
-			/*
-				sSend := tgbotapi.NewVoiceUpload(chatid, "E:\\GoApp\\src\\voice.dat")
-				_, err := bot.Send(sSend)
-				if err != nil {
-					log.Print(err.Error())
-				}
-			*/
-
-			/*
-				sSend := tgbotapi.NewDocumentUpload(chatid, "E:\\GoApp\\src\\document.doc")
-				_, err := bot.Send(sSend)
-				if err != nil {
-					log.Print(err.Error())
-				}
-			*/
+			go SendDocument(bot, chatid, "D:\\DaeseongGolang\\src\\file_48.doc")
 
 		} else if update.Message.Audio != nil {
 
-			url, err := bot.GetFileDirectURL(update.Message.Audio.FileID)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			fmt.Println(url)
-
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			file, err := os.OpenFile("audio.mp3", os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				continue
-			}
-			bytes, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				file.Close()
-				continue
-			}
-			file.Write(bytes)
-			file.Close()
+			go AudioDownload(bot, update.Message)
 
 		} else if update.Message.Photo != nil {
 
-			photos := *update.Message.Photo
-			photoId := photos[1].FileID
-			url, err := bot.GetFileDirectURL(photoId)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			fmt.Println(url)
-
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			file, err := os.OpenFile("photo.jpg", os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				continue
-			}
-			bytes, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				file.Close()
-				continue
-			}
-			file.Write(bytes)
-			file.Close()
+			go PhotoDownload(bot, update.Message)
 
 		} else if update.Message.Document != nil {
 
-			url, err := bot.GetFileDirectURL(update.Message.Document.FileID)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			fmt.Println(url)
-
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			file, err := os.OpenFile("document.doc", os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				continue
-			}
-			bytes, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				file.Close()
-				continue
-			}
-			file.Write(bytes)
-			file.Close()
+			go DocumentDownload(bot, update.Message)
 
 		} else if update.Message.Sticker != nil {
 
-			url, err := bot.GetFileDirectURL(update.Message.Sticker.FileID)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			fmt.Println(url)
-
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			file, err := os.OpenFile("sticker.tgs", os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				continue
-			}
-			bytes, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				file.Close()
-				continue
-			}
-			file.Write(bytes)
-			file.Close()
+			go StickerDownload(bot, update.Message)
 
 		} else if update.Message.Video != nil {
 
-			url, err := bot.GetFileDirectURL(update.Message.Video.FileID)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			fmt.Println(url)
-
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			file, err := os.OpenFile("video.mp4", os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				continue
-			}
-			bytes, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				file.Close()
-				continue
-			}
-			file.Write(bytes)
-			file.Close()
+			go VideoDownload(bot, update.Message)
 
 		} else if update.Message.Voice != nil {
 
-			url, err := bot.GetFileDirectURL(update.Message.Voice.FileID)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			//fmt.Println(url)
-
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			file, err := os.OpenFile("voice.dat", os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				continue
-			}
-			bytes, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println(err.Error())
-				resp.Body.Close()
-				file.Close()
-				continue
-			}
-			file.Write(bytes)
-			file.Close()
+			go VoiceDownload(bot, update.Message)
 
 		} else if update.Message.Location != nil {
 
-			fmt.Println(update.Message.Location.Latitude)
-			fmt.Println(update.Message.Location.Longitude)
+			go func() {
+				fmt.Println(update.Message.Location.Latitude)
+				fmt.Println(update.Message.Location.Longitude)
+			}()
 
 		} else if update.CallbackQuery != nil {
 
@@ -353,12 +464,14 @@ func main() {
 
 		} else {
 
-			sMsg := "정의된 메시지 내용이 아닙니다."
-			sSendMsg := tgbotapi.NewMessage(chatid, sMsg)
-			_, err := bot.Send(sSendMsg)
-			if err != nil {
-				log.Print(err.Error())
-			}
+			go func() {
+				sMsg := "정의된 메시지 내용이 아닙니다."
+				sSendMsg := tgbotapi.NewMessage(chatid, sMsg)
+				_, err := bot.Send(sSendMsg)
+				if err != nil {
+					log.Print(err.Error())
+				}
+			}()
 
 		}
 
